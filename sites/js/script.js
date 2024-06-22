897dacd47eee51893956b35babc90a47c6a5a6ed
vmaca <v.macalskux@yandex.ru>
2024-06-22 12:06:57 +0500

    18.06.24

---
+++ sites/js/script.js
@@ -0,0 +1,35 @@
+let currentIndex = 0;
+const slides = document.querySelectorAll('.slide');
+const slideInterval = 5000;
+ 
+function changeSlide() {
+  slides[currentIndex].style.opacity = 0;
+  currentIndex = (currentIndex + 1) % slides.length;
+  slides[currentIndex].style.opacity = 3;
+}
+ 
+setInterval(changeSlide, slideInterval);
+
+
+$(document).ready(function() {
+
+  function toggleSidebar() {
+    $(".button").toggleClass("active");
+    $("main").toggleClass("move-to-left");
+    $(".sidebar-item").toggleClass("active");
+  }
+
+  $(".button").on("click tap", function() {
+    toggleSidebar();
+  });
+
+  $(document).keyup(function(e) {
+    if (e.keyCode === 27) {
+      toggleSidebar();
+    }
+  });
+
+});
+
+
+
